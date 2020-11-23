8c8571945669915f129bed156900a369277fd85f
Roman Dronow <kane.piligrim@gmail.com>
2020-11-23 19:39:13 +0600

    Add solution (still not passes all tests) for Mastermind game

---
+++ src/main/kotlin/mastermind/evaluateGuess.kt
@@ -0,0 +1,43 @@
+package mastermind
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun charMap(str: String): HashMap<Char, Int> {
+    val map = HashMap<Char, Int>()
+    str.forEach { char: Char ->
+        if (!map.containsKey(char)) {
+            map[char] = 1
+        } else {
+            val prev = map[char]!!
+            map[char] = prev + 1
+        }
+    }
+    return map
+}
+
+fun HashMap<Char, Int>.changeCount(char: Char) {
+    val prev = this[char]!!
+    this[char] = prev - 1
+    if(this[char]!! < 0) throw Exception("Size is negative, boi")
+}
+
+fun HashMap<Char,Int>.checkLetterAvailable(char: Char): Boolean {
+    return this[char]!! > 0
+}
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    var right = 0
+    var wrong = 0
+    val secretMap = charMap(secret)
+    guess.forEachIndexed { index, char ->
+        if (char == secret[index]) {
+            right++
+            secretMap.changeCount(char)
+        }
+        if (secret.contains(char) && secret[index] != char && secretMap.checkLetterAvailable(char)) {
+            wrong++
+            secretMap.changeCount(char)
+        }
+    }
+    return Evaluation(right, wrong)
+}
