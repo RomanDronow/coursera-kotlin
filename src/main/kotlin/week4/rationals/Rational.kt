73aa6bcc64261ac05cebc48e319e97341e0322fa
Roman Dronow <kane.piligrim@gmail.com>
2020-12-15 20:09:40 +0600

    class Rational is done

--- src/main/kotlin/week4/rationals/Rational.kt
+++ src/main/kotlin/week4/rationals/Rational.kt
@@ -1,37 +1,136 @@
 package rationals
 
+import java.math.BigInteger
 
+data class Rational(var numerator: BigInteger, var denominator: BigInteger) : Comparable<Rational> {
+    init {
+        if (denominator.equals(0)) {
+            throw IllegalArgumentException("Denominator cannot be equals to zero")
+        }
 
-fun main() {
-    val half = 1 divBy 2
-    val third = 1 divBy 3
+        if (denominator.compareTo(BigInteger.ZERO) == -1) {
+            numerator *= BigInteger.valueOf(-1)
+            denominator *= BigInteger.valueOf(-1)
+        }
+        val (num, den) = normalize(numerator, denominator)
+        this.numerator = num
+        this.denominator = den
+    }
 
-    val sum: Rational = half + third
-    println(5 divBy 6 == sum)
+    constructor() : this(0, 1)
 
-    val difference: Rational = half - third
-    println(1 divBy 6 == difference)
+    constructor(numerator: Long, denominator: Long) : this(BigInteger.valueOf(numerator), BigInteger.valueOf(denominator))
 
-    val product: Rational = half * third
-    println(1 divBy 6 == product)
+    operator fun plus(other: Rational): Rational {
+        val numerator = this.numerator * other.denominator + other.numerator * denominator
+        val denominator = this.denominator * other.denominator
+        return Rational(numerator, denominator)
 
-    val quotient: Rational = half / third
-    println(3 divBy 2 == quotient)
+    }
 
-    val negation: Rational = -half
-    println(-1 divBy 2 == negation)
+    operator fun minus(other: Rational): Rational {
+        val numerator = this.numerator * other.denominator - other.numerator * denominator
+        val denominator = this.denominator * other.denominator
+        return Rational(numerator, denominator)
 
-    println((2 divBy 1).toString() == "2")
-    println((-2 divBy 4).toString() == "-1/2")
-    println("117/1098".toRational().toString() == "13/122")
+    }
 
-    val twoThirds = 2 divBy 3
-    println(half < twoThirds)
+    operator fun plusAssign(other: Rational) {
+        this.numerator = this.numerator * other.denominator + other.numerator * denominator
+        this.denominator = this.denominator * other.denominator
+    }
 
-    println(half in third..twoThirds)
+    operator fun minusAssign(other: Rational) {
+        this.numerator = this.numerator * other.denominator - other.numerator * denominator
+        this.denominator = this.denominator * other.denominator
+    }
 
-    println(2000000000L divBy 4000000000L == 1 divBy 2)
+    operator fun times(other: Rational): Rational {
+        return Rational(this.numerator * other.numerator, this.denominator * other.denominator)
+    }
 
-    println("912016490186296920119201192141970416029".toBigInteger() divBy
-            "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2)
+    operator fun div(other: Rational): Rational {
+        return this * Rational(other.denominator, other.numerator)
+    }
+
+    operator fun timesAssign(other: Rational) {
+        this.numerator = this.numerator * other.numerator
+        this.denominator = this.denominator * other.denominator
+    }
+
+    operator fun divAssign(other: Rational) {
+        this.numerator = this.numerator * other.denominator
+        this.denominator = this.denominator * other.numerator
+    }
+
+    operator fun unaryMinus(): Rational {
+        return Rational(this.numerator * BigInteger.valueOf(-1), this.denominator)
+    }
+
+    override fun toString(): String {
+        if (denominator.equals(BigInteger.ONE)) return "$numerator"
+        return "$numerator/$denominator"
+    }
+
+    override operator fun compareTo(other: Rational): Int {
+        val left = this.numerator * other.denominator
+        val right = other.numerator * denominator
+        if (left > right) return 1
+        if (right > left) return -1
+        return 0
+    }
+
+    operator fun rangeTo(other: Rational): ClosedRange<Rational> {
+        return object : ClosedRange<Rational> {
+            override val endInclusive: Rational = other
+            override val start: Rational = this@Rational
+        }
+    }
+}
+
+private fun normalize(one: BigInteger, another: BigInteger): Pair<BigInteger, BigInteger> {
+    fun euclid(a: BigInteger, b: BigInteger): BigInteger {
+        var copyA = a
+        var copyB = b
+        while (copyB != BigInteger.ZERO) {
+            val tmp = copyA % copyB
+            copyA = copyB
+            copyB = tmp
+        }
+        return copyA
+    }
+
+    val absOne = bigIntAbs(one)
+    val absAnother = bigIntAbs(another)
+
+    val gcd = if (absOne > absAnother) euclid(absOne, absAnother) else euclid(absAnother, absOne)
+    return Pair(one / gcd, another / gcd)
+}
+
+infix fun Number.divBy(other: Number): Rational {
+    if (!checkNumber(this) && !checkNumber(other)) {
+        throw Exception("Floating points values are forbidden")
+    }
+    return Rational(this.toLong(), other.toLong())
+}
+
+fun String.toRational(): Rational {
+    if (!this.contains('/')) return Rational(this.toBigInteger(), BigInteger.ONE)
+    val (numerator, denominator) = this.split('/')
+    return Rational(numerator.toBigInteger(), denominator.toBigInteger())
+}
+
+fun bigIntAbs(value: BigInteger): BigInteger {
+    return if (value.compareTo(BigInteger.ZERO) == -1) value * BigInteger.valueOf(-1) else value
+}
+
+private fun checkNumber(number: Number): Boolean {
+    return when (number) {
+        is Double -> false
+        is Float -> false
+        is Long -> true
+        is Int -> true
+        is BigInteger -> true
+        else -> false
+    }
 }
